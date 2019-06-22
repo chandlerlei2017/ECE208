b3b6ddbc01eea128d01d0849d856fa44884cbb4d
chandlerlei2017 <chandlerlei87@gmail.com>
2019-06-22 14:12:20 -0400

    updates

--- Q2_DPLL.cpp
+++ Q2_DPLL.cpp
@@ -3,8 +3,11 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+
+enum bcp_status{sat, unsat, cont};
 
 // Use to debug program
 
@@ -17,8 +20,51 @@ void print_vector( vector<vector<int>> temp ){
     }
     cout << "]" ;
     if ( i != temp.size() - 1) cout << ", ";
-    else cout << endl;
+    else cout << endl << endl;
   }
+}
+
+bool in_vector ( vector<int> vec, int item ) {
+  vector<int>::iterator it;
+  it = find(vec.begin(), vec.end(), item);
+
+  if(it == vec.end()) return false;
+  return true;
+}
+
+enum bcp_status BCP(  vector< vector< int > >& formula, int var) {
+  vector< vector< int > > copy = formula;
+
+  for ( int i = 0; i < copy.size(); i++ ) {
+
+    if (in_vector(copy[i], var)) {
+      copy.erase(copy.begin() + i);
+      i--;
+    }
+    else if(in_vector(copy[i], -var) && copy[i].size() != 0) {
+      copy[i].erase(find(copy[i].begin(), copy[i].end(), -var));
+    }
+    else if(in_vector(copy[i], -var)) {
+      return unsat;
+    }
+  }
+
+  if (copy.size() == 0) return sat;
+
+  formula = copy;
+  return cont;
+}
+
+string DPLL(  vector<vector<int>> formula, int assn ) {
+  print_vector(formula);
+
+  enum bcp_status res = BCP(formula, assn);
+
+  if ( res == sat ) return "SAT";
+  else if ( res == unsat ) return "UNSAT";
+
+  if ( DPLL( formula, formula[0][0]) == "SAT" ) return "SAT";
+  return DPLL( formula, -formula[0][0] );
 }
 
 int main()
@@ -47,9 +93,9 @@ int main()
   }
   file.close();
 
+  string ret = DPLL(formula, formula[0][0]);
 
-
-  // print_vector( formula );
+  cout << "This Formula is " << ret << endl;
 
   return 0;
 }
